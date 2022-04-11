6c9b61810d6284a331f4bcf1b31eded8203dac55
GMNSA <kohotokk@mail.ru>
2022-04-11 20:36:16 +0300

    main | create

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
+int my_pow(int a, int b);
 
 int main(void)
 {
-	puts("OK");
+	printf("result: %d\n", my_pow(8, 16));
 	return 0;
 }
 
+int my_pow(int a, int b) {
+	return (a * b);
+}
